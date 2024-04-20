fb43501c54ba5d94d408aeebd7e4b951c4b01483
yarasam <134842746+yarasam@users.noreply.github.com>
2024-04-20 17:56:21 +0000

    first commit

--- main.py
+++ main.py
@@ -39,27 +39,33 @@ def EDA(data):
         st.pyplot()
 
 def train(data, targetColumn):
+    #setup data
     setup(data=data, target=targetColumn,train_size=0.75, session_id=10,remove_outliers=True, normalize=True,normalize_method='minmax', transform_target=True)
-
     if data[targetColumn].dtype == 'object':
         #classification
         problemType = 'classification'
-        print('classification problem')
+        st.write('classification problem')
         model_options = ['lr', 'knn', 'ridge', 'lasso', 'dt', 'rf', 'xgboost']
     else:
         #regression
         problemType = 'regression'
-        print('regression problem')
+        st.write('regression problem')
         model_options = ['lr', 'knn', 'nb', 'dt', 'rf', 'xgboost']
+    #select modle
+    selected_model = st.radio("Select Model", model_options)
 
-    #select model
-    selected_model = st.selectbox("Select Model", model_options)
-    #train selected model
-    model = create_model(selected_model)
-    #finalize model
-    final_model = finalize_model(model)
-    #print model
-    st.write("model selected:", model)
+    print("Selected model:1", selected_model)
+    # Initialize final_model variable
+    final_model = None 
+    
+    if selected_model:
+        # train selected model
+        model = create_model(selected_model)
+        # finalize model
+        final_model = finalize_model(model)
+        # print model
+        st.write("model selected:", model)
+        print("Selected model2:", selected_model)
 
     return final_model,problemType
 
@@ -80,37 +86,40 @@ def test(model, X_test, y_test,problemType):
         st.write("Root Mean Squared Error:", np.sqrt(mean_squared_error(y_test, predictions['Label'])))
         st.write("R-squared:", r2_score(y_test, predictions['Label']))
 
-#getting data
-def main():
-    print('hello')
-    # app title
-    st.title("Machine Learning Model Input Form")
-    # add a header
-    st.header("Input Parameters")
-    #prompt the user to upload the dataset
-    uploaded_file = st.file_uploader("Upload file")
-    #target column
-    target_column_name = st.text_input("Enter the name of the target column:", value='targetColumn')
-    #add a button to submit the form
-    submit_button = st.button("Submit")
 
-    # process input when its Submited
-    if submit_button:
-        try:
-            # upload
-            upF = uploaded_file
-            df = get_data(upF)
-            st.write("Input DataFrame:")
-            st.write(df)
-            #call the functions
-            EDA(df)
-            model,problem = train(df,target_column_name)
-            X = df.drop(target_column_name, axis=1)
-            y = df[target_column_name]
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-            test(model, X_test, y_test, problem)
-        except Exception as e:
-            st.error(f"Error loading dataset: {e}")
+print('hello')
+# app title
+st.title("Machine Learning Model Input Form")
+# add a header
+st.header("Input Parameters")
+#prompt the user to upload the dataset
+uploaded_file = st.file_uploader("Upload file")
+#target column
+target_column_name = st.text_input("Enter the name of the target column:", value='targetColumn')
+#add a button to submit the form
+submit_button = st.button("Submit")
 
-    if __name__ == "__main__":
-        main()
+# process input when its Submited
+if submit_button:
+    try:
+         # upload
+        file_extension = uploaded_file.name.split(".")[-1]
+        if file_extension == "csv":
+            df = pd.read_csv(uploaded_file)
+        elif file_extension in ["xls", "xlsx"]:
+            df = pd.read_excel(uploaded_file)
+        elif file_extension == "json":
+            df = pd.read_json(uploaded_file)
+        else:
+            st.error("Unsupported file format. Please upload a CSV, Excel, or JSON file.")
+        st.write("Input DataFrame:")
+        st.write(df)
+         #call the functions
+        EDA(df)
+        model,problem = train(df,target_column_name)
+        X = df.drop(target_column_name, axis=1)
+        y = df[target_column_name]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        test(model, X_test, y_test, problem)
+    except Exception as e:
+        st.error(f"Error loading dataset: {e}")
