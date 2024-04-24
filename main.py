5cb7b3fc1b4d81afe9b74c134d6ca6f2912d7144
yarasam <134842746+yarasam@users.noreply.github.com>
2024-04-24 01:30:16 +0000

    sec commet (final)

--- main.py
+++ main.py
@@ -11,6 +11,21 @@ import numpy as np
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+def uploadfile(file):
+    file_extension = file.name.split(".")[-1]
+    if file_extension == "csv":
+        df = pd.read_csv(file)
+    elif file_extension in ["xls", "xlsx"]:
+        df = pd.read_excel(file)
+    elif file_extension == "json":
+        df = pd.read_json(file)
+    else:
+        st.error("Unsupported file format. Please upload a CSV, Excel, or JSON file.")
+        return None
+    df.head()
+    return df
+# function for EDA and data summary
 def EDA(data):
     st.subheader("Exploratory Data Analysis")
 
@@ -28,7 +43,7 @@ def EDA(data):
     sns.pairplot(data[numericCols])
     st.pyplot()
     #count plots for categorical columns
-    st.write("count plot for categorical columns:")
+    st.write("count plot for categorical columns (if any): ")
     for col in categoricalCols:
         plt.figure(figsize=(8, 6))
         sns.countplot(data[col])
@@ -37,89 +52,77 @@ def EDA(data):
         plt.ylabel("Count")
         #show on web app
         st.pyplot()
-
-def train(data, targetColumn):
-    #setup data
-    setup(data=data, target=targetColumn,train_size=0.75, session_id=10,remove_outliers=True, normalize=True,normalize_method='minmax', transform_target=True)
-    if data[targetColumn].dtype == 'object':
-        #classification
+#function to split data   
+def dataSplit(data):
+    data_sample = data.sample(frac=0.95, random_state=786)
+    data_unseen = data.drop(data_sample.index)
+    return data_sample,data_unseen
+#function set up data  
+def dataSetup(data, targetVar):
+    unique_values = data[targetVar].unique()
+    #if there is binery uniqe values
+    if len(unique_values) <= 2:  
         problemType = 'classification'
-        st.write('classification problem')
-        model_options = ['lr', 'knn', 'ridge', 'lasso', 'dt', 'rf', 'xgboost']
-    else:
-        #regression
+        st.write('classification')
+        #find categorical data
+        columns_with_0_1 = []
+        for column in data.columns:
+            if all(value in [0, 1] for value in data[column]):
+                columns_with_0_1.append(column)
+        columns_with_0_1.remove(targetVar)
+        st.write(columns_with_0_1)
+        return problemType, setup(data=data, target=targetVar, categorical_features=columns_with_0_1, session_id=123) 
+    # If the target variable is regression
+    elif data[targetVar].dtype in ['int64', 'float64']:
         problemType = 'regression'
-        st.write('regression problem')
-        model_options = ['lr', 'knn', 'nb', 'dt', 'rf', 'xgboost']
-    #select modle
-    selected_model = st.radio("Select Model", model_options)
+        st.write('regression')
+        return problemType, setup(data=data, target=targetVar, session_id=123)
 
-    print("Selected model:1", selected_model)
-    # Initialize final_model variable
-    final_model = None 
-    
-    if selected_model:
-        # train selected model
-        model = create_model(selected_model)
-        # finalize model
-        final_model = finalize_model(model)
-        # print model
-        st.write("model selected:", model)
-        print("Selected model2:", selected_model)
-
-    return final_model,problemType
-
-def test(model, X_test, y_test,problemType):
-
-    st.subheader("Model Evaluation")
-
-    if problemType == 'classification':
-        predictions = predict_model(model, data=X_test)
-        st.write("Classification Report:")
-        st.write(classification_report(y_test, predictions['Label']))
-        st.write("Confusion Matrix:")
-        st.write(confusion_matrix(y_test, predictions['Label']))
     else:
-        predictions = predict_model(model, data=X_test)
-        st.write("Mean Absolute Error:", mean_absolute_error(y_test, predictions['Label']))
-        st.write("Mean Squared Error:", mean_squared_error(y_test, predictions['Label']))
-        st.write("Root Mean Squared Error:", np.sqrt(mean_squared_error(y_test, predictions['Label'])))
-        st.write("R-squared:", r2_score(y_test, predictions['Label']))
+        st.write('Unable to determine the type of problem. Further inspection may be needed.')
+        return None, None
+    
+#show predections
+def test_predict_model (model, data):
+    predictions = predict_model(model,data=data)
+    st.write(predictions.head())
 
+# MAIN
 
-print('hello')
-# app title
-st.title("Machine Learning Model Input Form")
-# add a header
+#app title
+st.title("Machine Learning Model")
+#add a header
 st.header("Input Parameters")
 #prompt the user to upload the dataset
-uploaded_file = st.file_uploader("Upload file")
-#target column
-target_column_name = st.text_input("Enter the name of the target column:", value='targetColumn')
-#add a button to submit the form
-submit_button = st.button("Submit")
+file_path = st.file_uploader("input file name with extention")
+#target variable
+target_column_name = st.text_input("Enter the name of the target column:", value='target Column')
+#colomns to delete
+delete_col = st.text_input("Enter column name you want to delete (optional) : ", value='delete columns')
+submit = st.button("Submit")
 
 # process input when its Submited
-if submit_button:
-    try:
-         # upload
-        file_extension = uploaded_file.name.split(".")[-1]
-        if file_extension == "csv":
-            df = pd.read_csv(uploaded_file)
-        elif file_extension in ["xls", "xlsx"]:
-            df = pd.read_excel(uploaded_file)
-        elif file_extension == "json":
-            df = pd.read_json(uploaded_file)
-        else:
-            st.error("Unsupported file format. Please upload a CSV, Excel, or JSON file.")
-        st.write("Input DataFrame:")
-        st.write(df)
-         #call the functions
-        EDA(df)
-        model,problem = train(df,target_column_name)
-        X = df.drop(target_column_name, axis=1)
-        y = df[target_column_name]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        test(model, X_test, y_test, problem)
-    except Exception as e:
-        st.error(f"Error loading dataset: {e}")
+if submit:
+    data = uploadfile(file_path)
+    if delete_col and delete_col in data.columns:
+        data.drop(columns=delete_col, inplace=True)
+    st.write(data)
+    st.write(data.shape)
+    EDA(data)
+    data_sample, data_test_set = dataSplit(data)
+    st.write('sample : ')
+    st.write(data_sample)
+    st.write('sample shape : ')
+    st.write(data_sample.shape)
+    st.write('test sample : ')
+    st.write(data_test_set)
+    st.write('test sample shape : ')
+    st.write(data_test_set.shape)
+    #set up model
+    problem_type, best_model = dataSetup(data_sample, target_column_name)
+    if problem_type:
+        #choose model & train
+        best_model_trained = compare_models() 
+        #test
+        test_predict_model(best_model_trained, data_test_set)
+
